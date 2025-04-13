77045464d123d94eb7a57d2a4e80daa903c1a70f
hjsh200219 <ho.shin@teoul.com>
2025-04-14 08:18:49 +0900

    설정 페이지

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,19 +1,19 @@
-import Layout from '@/app/layout';
 import PageHeader from '@/components/UI/PageHeader';
+import Layout from '@/components/Layout';
 
 export default function Settings() {
   return (
     <Layout>
-      <div className="container mx-auto px-4 py-4">
-        <div className="flex justify-between items-center mb-4">
-          <PageHeader 
-            title="설정" 
-          />
-        </div>
-        <div className="bg-gray-800 rounded-lg shadow-md p-6">
-          <p>설정 페이지입니다.</p>
-        </div>
+    <div className="container mx-auto px-4 py-4">
+      <div className="flex justify-between items-center mb-4">
+        <PageHeader 
+          title="설정" 
+        />
       </div>
+      <div className="bg-gray-800 rounded-lg shadow-md p-6">
+        <p>설정 페이지입니다.</p>
+      </div>
+    </div>
     </Layout>
   );
 } 
